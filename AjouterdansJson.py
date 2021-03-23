5acb362fee70ce29cfbf94bee2caa4d1868a2ad1
dragon7653 <david.febry@gmail.com>
2021-03-23 16:17:36 +0100

    Ajout de commentaire json

--- AjouterdansJson.py
+++ AjouterdansJson.py
@@ -4,8 +4,12 @@ chemin = "/home/dragon76/Documents/ProjetPython/fichier.json"
 
 with open("fichier.json", "r") as f:
     donnees = json.load(f)
-
-donnees.append(4)
+nom = "Durand"
+prenom = "David"
+ville = "Angers"
+donnees.extend([{"nom" : nom, 
+                 "prenom" : prenom,
+                 "ville" : ville}])
 
 
 with open("fichier.json", "w") as f:
